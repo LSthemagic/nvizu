a9bef53d6c535f6a6c838769c67eb82ccc9dc41d
Railan Santana <lansilvah14fsa@gmail.com>
2024-07-17 10:33:21 -0300

    remake project for dom black

--- src/client/home/Home.jsx
+++ src/client/home/Home.jsx
@@ -1,50 +1,46 @@
-import { FaAndroid, FaAppStore } from "react-icons/fa"
+import { FaAndroid, FaAppStore } from "react-icons/fa";
 
 const Home = () => {
     return (
         <div className="home">
             <video
-                src="/background.mp4"
+                src="/dom_black.mp4"
                 loop
                 autoPlay
                 muted
                 playsInline
                 className="background-video"
-            >
-            </video>
+            ></video>
+
             <div className="home-content">
+                <nav>
+                    <img
+                        src="/logo2.png"
+                        width="175px"
+                        alt="Logo"
+                    />
+                </nav>
                 <div className="content p-10">
-                    <nav>
-                        <img
-                            className="m-3 mb-5 mt-0"
-                            src="/NVIZU.png"
+                    <section className="grid grid-cols-1 gap-2 text-justify w-full">
+                        <a
+                            className="flex flex-row gap-5 p-3 pl-10 pr-10 rounded-lg items-center bg-primary text-white hover:scale-105 hover:text-secondary"
+                            href="https://play.google.com/store/apps/details?id=br.com.starapp.appbarbercli&pcampaignid=web_share"
                         >
-
-                        </img>
-                    </nav>
-                    <section className="grid grid-col gap-2 text-justify w-full">
-                            <a
-                                className="p-3 rounded-lg flex flex-row justify-start items-center gap-2 bg-primary text-secondary "
-                                href="https://play.google.com/store/apps/details?id=br.com.starapp.appbarbercli&pcampaignid=web_share"
-                            >
-                                Agendar no ANDROID
-                                <FaAndroid />
-                            </a>
-                            <a
-                                className="p-3 rounded-lg flex flex-row justify-start items-center gap-2 bg-primary text-secondary "
-                                href="https://apps.apple.com/br/app/appbarber-cliente/id6450795073"
-                            >
-                                
-                                Agendar no IOS
-                                <FaAppStore />
-                            </a>
-                           
+                            <FaAndroid />
+                            Baixar para ANDROID
+                        </a>
+                        <a
+                            className="flex flex-row gap-5 p-3 pl-10 pr-10 rounded-lg items-center bg-primary text-white hover:scale-105 hover:text-secondary"
+                            href="https://apps.apple.com/br/app/appbarber-cliente/id6450795073"
+                        >
+                            <FaAppStore />
+                            Baixar para IOS
+                        </a>
                     </section>
-
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
